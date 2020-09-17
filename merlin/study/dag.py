b31224adc0bc2581420d39d8f24afbfeaf7c9669
Benjamin Bay <bay1@llnl.gov>
2020-09-17 12:19:11 -0700

    updated dag.py

--- merlin/study/dag.py
+++ merlin/study/dag.py
@@ -6,7 +6,7 @@
 #
 # LLNL-CODE-797170
 # All rights reserved.
-# This file is part of Merlin, Version: 1.7.4.
+# This file is part of Merlin, Version: 1.7.3.
 #
 # For details, see https://github.com/LLNL/merlin.
 #
@@ -28,42 +28,37 @@
 # SOFTWARE.
 ###############################################################################
 
-"""
-Holds DAG class. TODO make this an interface, separate from Maestro.
-"""
 from collections import OrderedDict
 
-from merlin.study.step import Step
+from merlin.study.step import MerlinStepRecord, Step
 
 
 class DAG:
     """
     This class provides methods on a task graph that Merlin needs for staging
-    tasks in celery. It is initialized from am maestro ExecutionGraph, and the
-    major entry point is the group_tasks method, which provides groups of
-    independent chains of tasks.
+    tasks in celery. The major entry point is the group_tasks method, which
+    provides groups of independent chains of tasks.
     """
 
-    def __init__(self, maestro_dag, labels):
-        """
-        :param `maestro_dag`: A maestrowf ExecutionGraph.
-        """
-        self.dag = maestro_dag
+    def __init__(self, value_dag, labels):
+        self.dag = value_dag
         self.backwards_adjacency = {}
         self.calc_backwards_adjacency()
         self.labels = labels
 
     def step(self, task_name):
         """Return a Step object for the given task name
-
         :param `task_name`: The task name.
         :return: A Merlin Step object.
         """
-        return Step(self.dag.values[task_name])
+        # step_dict = self.dag.values[task_name]
+        # workspace_value = ""
+        # merlin_step_record = MerlinStepRecord(workspace_value, step_dict)
+        # return Step(merlin_step_record) # TODO is this okay?
+        return self.dag.values[task_name]
 
     def calc_depth(self, node, depths, current_depth=0):
         """Calculate the depth of the given node and its children.
-
         :param `node`: The node (str) to start at.
         :param `depths`: the dictionary of depths to update.
         :param `current_depth`: the current depth in the graph traversal.
@@ -79,17 +74,12 @@ class DAG:
     @staticmethod
     def group_by_depth(depths):
         """Group DAG tasks by depth.
-
         :param `depths`: the dictionary of depths to group by
-
         :return: a list of lists of lists ordered by depth
-
         ([[["tasks"],["with"],["Depth 0"]],[["tasks"],["with"],["Depth 1"]]])
-
         The outer index of this list is the depth, the middle index is which
         chain of tasks in that depth, and the inner index is the task id in
         that chain.
-
         This method only groups by depth, and has one task in every chain.
         find_independent_chains is used to figure out how to coalesce chains
         across depths.
@@ -113,15 +103,13 @@ class DAG:
     def children(self, task_name):
         """ Return the children of the task.
         :param `task_name`: The name of the task to get the children of.
-
         :return: list of children of this task.
         """
-        return self.dag.adjacency_table[task_name]
+        return list(self.dag.get_adjacency_matrix()[task_name])
 
     def num_children(self, task_name):
         """ Find the number of children for the given task in the dag.
         :param `task_name`: The name of the task to count the children of.
-
         :return : number of children this task has
         """
         return len(self.children(task_name))
@@ -129,14 +117,12 @@ class DAG:
     def parents(self, task_name):
         """ Return the parents of the task.
         :param `task_name` : The name of the task to get the parents of.
-
         :return : list of parents of this task"""
         return self.backwards_adjacency[task_name]
 
     def num_parents(self, task_name):
         """find the number of parents for the given task in the dag
         :param `task_name` : The name of the task to count the parents of
-
         :return : number of parents this task has"""
         return len(self.parents(task_name))
 
@@ -146,7 +132,6 @@ class DAG:
         :param `task_name` : The task to search for.
         :param `list_of_groups_of_chains` : list of groups of chains to search
             for the task
-
         :return : the list representing the chain containing task_name"""
         for group in list_of_groups_of_chains:
             for chain in group:
@@ -156,8 +141,11 @@ class DAG:
 
     def calc_backwards_adjacency(self):
         """ initializes our backwards adjacency table """
-        for parent in self.dag.adjacency_table:
-            for task_name in self.dag.adjacency_table[parent]:
+        adj_matrix = self.dag.get_adjacency_matrix()
+        print(adj_matrix)
+        for parent in adj_matrix:
+            print(parent)
+            for task_name in adj_matrix[parent]:
                 if task_name in self.backwards_adjacency:
                     self.backwards_adjacency[task_name].append(parent)
                 else:
@@ -177,21 +165,15 @@ class DAG:
         """
         Finds independent chains and adjusts with the groups of chains to
         maximalize parallelism
-
         :param list_of_groups_of_chains: List of list of lists, as returned by
             self.group_by_depth
-
         e.g.,
-
         ([[["task1"],["with"],["Depth 0"]],[["task2"],["has"],["Depth 1"]]])
-
         :return : This takes the groups of chains and looks for opportunities
             to move tasks in deeper groups of chains into chains in shallower
             groups, thus increasing available parallelism in the execution.
-
             Depending on the precise parental relationships between the tasks
             in the graph the output may be something like:
-
             ([[["task1", "has"],["with","task2"],["Depth 0"]],["Depth 1"]]])
         """
         for group in list_of_groups_of_chains:
@@ -200,6 +182,10 @@ class DAG:
 
                     if self.num_children(task_name) == 1 and task_name != "_source":
 
+                        print("***self.children(task_name)")
+                        print(task_name)
+                        print(self.children)
+                        print(self.children(task_name))
                         child = self.children(task_name)[0]
 
                         if self.num_parents(child) == 1:
@@ -222,10 +208,8 @@ class DAG:
 
     def group_tasks(self, source_node):
         """Group independent tasks in a directed acyclic graph (DAG).
-
         Starts from a source node and works down, grouping tasks by
         depth, then identify independent parallel chains in those groups.
-
         :param dag : The DAG
         :param source_node: The source node.
         """

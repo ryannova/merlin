21c160e80f5dad063b58dce1cf5eaed2c494997a
Benjamin Bay <bay1@llnl.gov>
2021-01-05 11:24:34 -0700

    merlin restart

--- merlin/cli/commands/restart.py
+++ merlin/cli/commands/restart.py
@@ -1,4 +1,11 @@
 import click
+import glob
+import os
+
+from merlin import router
+from merlin.ascii_art import banner_small
+from merlin.cli.utils import verify_filepath
+from merlin.study.study import MerlinStudy
 
 
 @click.command()
@@ -16,4 +23,18 @@ def cli(restart_dir, local):
     """
     Restart a workflow using an existing Merlin workspace.
     """
-    print(f"restart at {restart_dir}. local={local}")
+    print(banner_small)
+    filepath = os.path.join(restart_dir, "merlin_info", "*.expanded.yaml")
+    possible_specs = glob.glob(filepath)
+    if len(possible_specs) == 0:
+        raise ValueError(
+            f"'{filepath}' does not match any provenance spec file to restart from."
+        )
+    elif len(possible_specs) > 1:
+        raise ValueError(
+            f"'{filepath}' matches more than one provenance spec file to restart from."
+        )
+    filepath = verify_filepath(possible_specs[0])
+    print(f"Restarting workflow at '{restart_dir}'")
+    study = MerlinStudy(filepath, restart_dir=restart_dir)
+    router.run_task_server(study, local)
